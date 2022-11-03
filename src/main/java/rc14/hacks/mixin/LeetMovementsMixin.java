514457651468e1ed9bdfc0caf98275fa8fc0881f
RC-14 <61058098+RC-14@users.noreply.github.com>
2022-11-03 14:51:32 +0100

    Improve LeetMovementsMixin

--- src/main/java/rc14/hacks/mixin/LeetMovementsMixin.java
+++ src/main/java/rc14/hacks/mixin/LeetMovementsMixin.java
@@ -1,29 +1,29 @@
 package rc14.hacks.mixin;
 
-import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
-@Mixin(ClientPlayerEntity.class)
+@Mixin(PlayerMoveC2SPacket.class)
 public class LeetMovementsMixin {
-	@Redirect(method = "sendMovementPackets()V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;getX()D"))
-	private double returnLeetX(ClientPlayerEntity player) {
-		double x = Math.abs(player.getX());
-		x -= x % 0.001;
-		x += 0.0001337;
-		if (player.getX() < 0) x = -x;
+	@ModifyVariable(method = "<init>(DDDFFZZZ)V", at = @At("HEAD"), ordinal = 0, argsOnly = true)
+	private static double returnLeetX(double x) {
+		double leetX = Math.abs(x);
+		leetX -= leetX % 0.001;
+		leetX += 0.0001337;
+		if (x < 0) leetX = -leetX;
 
-		return x;
+		return leetX;
 	}
 
-	@Redirect(method = "sendMovementPackets()V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;getZ()D"))
-	private double returnLeetZ(ClientPlayerEntity player) {
-		double z = Math.abs(player.getZ());
-		z -= z % 0.001;
-		z += 0.0001337;
-		if (player.getZ() < 0) z = -z;
+	@ModifyVariable(method = "<init>(DDDFFZZZ)V", at = @At("HEAD"), ordinal = 2, argsOnly = true)
+	private static double returnLeetZ(double z) {
+		double leetZ = Math.abs(z);
+		leetZ -= leetZ % 0.001;
+		leetZ += 0.0001337;
+		if (z < 0) leetZ = -leetZ;
 
-		return z;
+		return leetZ;
 	}
 }
